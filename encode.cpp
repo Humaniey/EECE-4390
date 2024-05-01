7d59113c608b1473ccf966eb17fa3735bc0b305d
Humanie <160002624+Humaniey@users.noreply.github.com>
2024-04-30 23:38:22 -0500

    Gave descriptions to coding schemes

--- encode.cpp
+++ encode.cpp
@@ -5,81 +5,137 @@
 
 using namespace std;
 
-// Maps binary 1 to postive voltage 
-// and binary 0 to negative voltage
+// NRZ-L encoding switches to negative (or positive) if there is a 1
+// then switches back to positive (or negative) if there is a 0
 string encodeNRZL(string input) {
+    // Set up the inital boundary
     string output = "|";
+    // Set up a loop to run through the length of the input code
     for(unsigned long i = 0; i < input.length(); i++) {
+        // Check if input is 1
         if(input[i] == '1')
+            // Display negative voltage
             output += "-V|";
+        // Check if input is 0
         else if(input[i] == '0')
+            // Display positive voltage
             output += "+V|";
     }
-
+    // Return the resulting waveform 
     return output;
 }
 
+// NRZ-I encoding transitions between positive and negative voltage wherever there is a 1 in the input.
+// If there is a 0, proceed to output the voltage previous.
 string encodeNRZI(string input) {
+    // Set up the inital boundary
     string output = "|";
+    // Set up a state of to reaffirm which way to transition
     bool state = 0;
+    // Set up a loop to run through the length of the input code
     for(unsigned long i = 0; i < input.length(); i++) {
+        // Check if the input is 1
         if (input[i] == '1')
+            // Switch the state of output voltage
             state = !state;
+        // Check if the state if true or false 
         if (state)
+            // Output a positive voltage and bound it
             output += "+V|";
+        // Else...    
         else
+            // Output a negative voltage and bound it
             output += "-V|";
     }
-    
+    // Return the resulting waveform
     return output;        
 }
 
+//B-AMI encoding transitions between positive and negative voltage wherever there is a 1 in the input.
+// If there is a 0, proceed to output no voltage or 0 volts.
 string encodeBAMI(string input) {
+    // Set up the inital boundary
     string output = "|";
+    // Set up a state of to reaffirm which way to transition
     bool state = 0;
+    // Set up a loop to run through the length of the input code
     for (unsigned long i = 0; i < input.length(); i++) {
+        // Check if input is 0
         if (input[i] == '0')
+            // Display 0 volts and bound it
             output += "0V|";
+        // Check if the input is 1
         else if (input[i] == '1') {
+            // Switch the state of output voltage
             state = !state;
+            // Check if the state if true or false
             if (state)
+                // Output a positive voltage and bound it
                 output += "+V|";
+            // Else...
             else
+                // Output a negative voltage and bound it
                 output += "-V|";
         }
     }
+    // Return the resulting waveform
     return output;
 }
-
+// Manchester encoding contains a transition between the clock cycle rather than at the start.
+// An input of 0 will transition from positive to negative while an input of 1 transitions from negative to positive voltage.
 string encodeManchester(string input) {
+    // Set up the inital boundary
     string output = "|";
+    // Set up a loop to run through the length of the input code
     for(unsigned long i = 0; i < input.length(); i++) {
+        // Check if the input is 1
         if(input[i] == '1')
+            // Return a negative to positive voltage
             output += "-V+V|";
-        else if(input[i] == '0')
+        // Else...
+        else
+            // Return a positive to negative voltage
             output += "+V-V|";
     }
+    // Return the resulting waveform
     return output;
 }
-
+// D-Manchester encoding contains a transition between the clock cycle rather than at the start.
+// With an input of 1, the voltage will be held from the previous cycle then transition.
+// An input of 0 will not hold the voltage and transition the same as the previous.
 string encodeDManchester(string input) {
+    // Set up the inital boundary
     string output = "|";
+    // Set up a state of to reaffirm which way to transition
     bool state = 0;
+    // Set up a loop to run through the length of the input code
     for (unsigned long i = 0; i < input.length(); i++) {
+        // Check if the input is 1
         if (input[i] == '1') {
+                // Switch the state of output voltage transition
                 state = !state;
+            // Check if the state if true or false
             if (state)
+                // Return a negative to positive voltage
                 output += "-V+V|";
+            // Else...    
             else
+                // Return a positive to negative voltage
                 output += "+V-V|";
             }
+        // Check if the input is 0
         else if (input[i] == '0') {
+            // Check if the state if true or false
             if (state)
+                // Return a negative to positive voltage
                 output += "-V+V|";
+            // Else...   
             else
+                // Return a positive to negative voltage 
                 output += "+V-V|";
             }
     }
+    // Return the resulting waveform
     return output;
 }
 
@@ -119,7 +175,7 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // Route encoding
+    // Route encoding to their own files with their own names
     string output;
     string outputFileName = "OUTPUT";
     cout << "Using encoding ";
@@ -155,7 +211,7 @@ int main(int argc, char* argv[]) {
             return 1;
     }
 
-    // Write result to output file
+    // Write result to output files
     ofstream ofs;
     ofs.open(outputFileName);
     ofs << output;
